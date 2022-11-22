dde578d10d4ab44affa21fe53a56aa79d41a460e
jbalsby <jbalsby@live.dk>
2022-11-22 12:16:52 +0100

    yesssiirrr

--- FirstSemesterExam/Projectiles/PlayerProjectile.cs
+++ FirstSemesterExam/Projectiles/PlayerProjectile.cs
@@ -1,27 +1,31 @@
 ﻿using FirstSemesterExam.Enemies;
+using FirstSemesterExam.Projectiles;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using SharpDX.Direct3D9;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FirstSemesterExam.Projectiles
+namespace FirstSemesterExam
 {
     public class PlayerProjectile : Projectile
     {
-        public PlayerProjectile(Vector2 playerPosition) : base(playerPosition)
+        public PlayerProjectile(Vector2 position, Vector2 velocity) : base(position)
         {
-            position = playerPosition;
+            this.velocity = velocity;
+            this.position = position;
             attackDamage = 10f;
+            speed = 1000;
         }
 
         public override void LoadContent(ContentManager content)
         {
             sprites = new Texture2D[1];
-            sprites[0] = content.Load<Texture2D>("");
+            sprites[0] = content.Load<Texture2D>("Projectile1");
         }
 
         public override void OnCollision(GameObject other)
