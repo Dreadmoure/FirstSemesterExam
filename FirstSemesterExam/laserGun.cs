25a23495d2e389c8711ea8bf12166e6b7e2e0a0c
jbalsby <jbalsby@live.dk>
2022-11-22 12:09:09 +0100

    yessir

--- FirstSemesterExam/laserGun.cs
+++ FirstSemesterExam/laserGun.cs
@@ -1,15 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FirstSemesterExam.Menu;
+using FirstSemesterExam.Projectiles;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace FirstSemesterExam
 {
     internal class laserGun : Weapon
     {
+
         public laserGun(Player player) : base(player)
         {
+
         }
+
+        public override void Shoot()
+        {
+            PlayerProjectile projectile = new PlayerProjectile(shootingPos, dirVector);
+            GameState.InstantiateGameObject(projectile);
+        }
+
     }
 }
