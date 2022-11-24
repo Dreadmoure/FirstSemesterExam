d7a7146d65e5c3aa36d7700ca9e5cd6d698a7a84
IdaDania <54858@student.eadania.dk>
2022-11-24 16:19:29 +0100

    make new state for showing highscores via main menu

---
+++ FirstSemesterExam/HighScore/Highscore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstSemesterExam.HighScore
+{
+    public class Highscore
+    {
+        // make list for scores 
+        public List<Score> scores = new List<Score>(); 
+
+        // read scores from file, and parse them into the list 
+
+
+        // sort the list according to scores - highest to lowest 
+
+
+        // rewrite the file in the order of the scores 
+
+    }
+}
---
+++ FirstSemesterExam/Menu/HighscoreState.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstSemesterExam.Menu
+{
+    public class HighscoreState : State
+    {
+        #region fields 
+        private Texture2D menuBackgroundTexture;
+        private List<Button> buttons;
+        private Button backButton;
+        #endregion
+
+        public HighscoreState(ContentManager content, GraphicsDevice graphicsDevice, GameWorld game) : base(content, graphicsDevice, game)
+        {
+            float buttonLayer = 0.2f;
+            float buttonScale = 6f;
+
+            backButton = new Button(new Vector2(GameWorld.GetScreenSize.X / 2, GameWorld.GetScreenSize.Y / 2 - GameWorld.GetScreenSize.Y / 6), "Back", buttonLayer, buttonScale);
+            
+            buttons = new List<Button>() { backButton };
+
+            LoadContent();
+        }
+
+        #region methods 
+        public override void LoadContent()
+        {
+            menuBackgroundTexture = content.Load<Texture2D>("Menus\\background");
+
+            foreach (Button button in buttons)
+            {
+                button.LoadContent(content);
+            }
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            foreach (Button button in buttons)
+            {
+                button.Update(gameTime);
+            }
+
+            if (backButton.isClicked)
+            {
+                backButton.isClicked = false;
+                game.ChangeState(GameWorld.GetMenuState);
+            }
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin(SpriteSortMode.FrontToBack, samplerState: SamplerState.PointClamp);
+
+            spriteBatch.Draw(menuBackgroundTexture, new Vector2(GameWorld.GetScreenSize.X / 2, GameWorld.GetScreenSize.Y / 2), null, Color.White, 0f, new Vector2(menuBackgroundTexture.Width / 2, menuBackgroundTexture.Height / 2), 6f, SpriteEffects.None, 0.1f);
+
+            foreach (Button button in buttons)
+            {
+                button.Draw(gameTime, spriteBatch);
+            }
+
+            spriteBatch.End();
+        }
+        #endregion
+    }
+}
