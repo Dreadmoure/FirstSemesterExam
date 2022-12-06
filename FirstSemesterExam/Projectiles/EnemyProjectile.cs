64134c827267c6b801f26b3fc6d11ef229acc0b9
denniwind <denniwind@live.dk>
2022-12-06 11:41:23 +0100

    more stuff

--- FirstSemesterExam/Projectiles/EnemyProjectile.cs
+++ FirstSemesterExam/Projectiles/EnemyProjectile.cs
@@ -12,9 +12,19 @@ using System.Threading.Tasks;
 
 namespace FirstSemesterExam.Projectiles
 {
+    /// <summary>
+    /// EnemyProjectile class, inherits from the superclass Projectile.
+    /// this is the projectile the enemies shoots
+    /// </summary>
     public class EnemyProjectile : Projectile
     {
         #region Constructors
+        /// <summary>
+        /// Constructor for the EnemyProjectile
+        /// </summary>
+        /// <param name="enemyPosition">inherited from the superclass, sets the position from where the projectile spawns/param>
+        /// <param name="playerPosition">sets the target position, where the shot needs to go towards</param>
+        /// <param name="enemyAttackRange">sets the enemies attack range</param>
         public EnemyProjectile(Vector2 enemyPosition, Vector2 playerPosition, float enemyAttackRange) : base(enemyPosition)
         {
             position = enemyPosition;
@@ -24,8 +34,9 @@ namespace FirstSemesterExam.Projectiles
             layerDepth = 0.6f;
             
 
-            // angle of projectile 
+            // sets the angle of the projectile 
             float angle = MathF.Atan2(playerPosition.Y - enemyPosition.Y, playerPosition.X - enemyPosition.X);
+            // converts it to a vector
             velocity = new Vector2(MathF.Cos(angle), MathF.Sin(angle));
             rotation = angle; 
         }
@@ -34,12 +45,16 @@ namespace FirstSemesterExam.Projectiles
         #region Methods
         public override void LoadContent(ContentManager content)
         {
+            //loads the sprite
             sprites = new Texture2D[1];
             sprites[0] = content.Load<Texture2D>("Projectiles\\Projectile1");
         }
 
         public override void OnCollision(GameObject other)
         {
+            //if the projectile collides with the player
+            //calls the takedamage method and passes on the attackdamage as a parameter
+            //then removes the projectile
             if (other is Player)
             {
                 other.TakeDamage(attackDamage);
--- FirstSemesterExam/Projectiles/Projectile.cs
+++ FirstSemesterExam/Projectiles/Projectile.cs
@@ -8,6 +8,9 @@ using System.Threading.Tasks;
 
 namespace FirstSemesterExam.Projectiles
 {
+    /// <summary>
+    /// abstract superclass for EnemyProjectile and PlayerProjectile
+    /// </summary>
     public abstract class Projectile : GameObject
     {
         #region Fields
@@ -16,6 +19,11 @@ namespace FirstSemesterExam.Projectiles
         #endregion
 
         #region Constructors
+        /// <summary>
+        /// Constructor for Projectile which takes a attackPosition as parameter
+        /// attackPosition indicates where it shoots from
+        /// </summary>
+        /// <param name="attackPosition"></param>
         public Projectile(Vector2 attackPosition)
         {
             initialAttackPosition = attackPosition;
@@ -40,8 +48,12 @@ namespace FirstSemesterExam.Projectiles
             Move(gameTime);
         }
 
+        /// <summary>
+        /// Checks if the projectile is outside of bounds, if it is, then it will be removed
+        /// </summary>
         private void CheckIfOutsideBounds()
         {
+            //min and max values for the boundingbox
             Vector2 min = new Vector2(GetSpriteSize.X / 2, GetSpriteSize.Y / 2);
             Vector2 max = new Vector2(GameWorld.GetScreenSize.X - GetSpriteSize.X / 2, GameWorld.GetScreenSize.Y - GetSpriteSize.Y / 2);
             if (position.X <= min.X || position.X >= max.X || position.Y <= min.Y || position.Y >= max.Y)
@@ -50,6 +62,9 @@ namespace FirstSemesterExam.Projectiles
             }
         }
 
+        /// <summary>
+        /// Checks to see if projectiles range is out of range, if it is, the projectile will be removed
+        /// </summary>
         private void CheckIfOutsideRange()
         {
             Vector2 min = new Vector2(initialAttackPosition.X - GetSpriteSize.X / 2 - attackRange, initialAttackPosition.Y - GetSpriteSize.Y / 2 - attackRange);
